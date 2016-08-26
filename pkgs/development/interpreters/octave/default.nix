b0763ed89f813d9d84b3cfee55a32de36562130b
timor <timor.dd@googlemail.com>
2016-08-26 02:01:26 +0200

    octave: add osmesa support
    Needs llvm.  In order for octave to build it, jit woud have to be
    enabled.  Patched configure to search for it regardless if osmesa is requested.

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -1,13 +1,15 @@
 { stdenv, fetchurl, gfortran, readline, ncurses, perl, flex, texinfo, qhull
-, libX11, graphicsmagick, pcre, pkgconfig, mesa, fltk
-, fftw, fftwSinglePrec, zlib, curl, qrupdate, openblas
+, libX11, graphicsmagick, pcre, pkgconfig, mesa, mesa_noglu, fltk
+, fftw, fftwSinglePrec, zlib, curl, qrupdate, openblas, makeWrapper
 , qt ? null, qscintilla ? null, ghostscript ? null, llvm ? null, hdf5 ? null,glpk ? null
-, suitesparse ? null, gnuplot ? null, jdk ? null, python ? null
+, suitesparse ? null, gnuplot ? null, jdk ? null, python ? null, osmesa ? null
 }:
 
 let
   suitesparseOrig = suitesparse;
   qrupdateOrig = qrupdate;
+  # if osmesa is used, use same llvm version as them
+  selected_llvm = if (osmesa != null) then mesa_noglu.passthru.llvm else llvm;
 in
 # integer width is determined by openblas, so all dependencies must be built
 # with exactly the same openblas
@@ -27,22 +29,30 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ gfortran readline ncurses perl flex texinfo qhull libX11
     graphicsmagick pcre pkgconfig mesa fltk zlib curl openblas
-    fftw fftwSinglePrec qrupdate ]
+    fftw fftwSinglePrec qrupdate makeWrapper ]
     ++ (stdenv.lib.optional (qt != null) qt)
     ++ (stdenv.lib.optional (qscintilla != null) qscintilla)
     ++ (stdenv.lib.optional (ghostscript != null) ghostscript)
-    ++ (stdenv.lib.optional (llvm != null) llvm)
+    ++ (stdenv.lib.optional (selected_llvm != null) selected_llvm)
     ++ (stdenv.lib.optional (hdf5 != null) hdf5)
     ++ (stdenv.lib.optional (glpk != null) glpk)
     ++ (stdenv.lib.optional (suitesparse != null) suitesparse)
     ++ (stdenv.lib.optional (jdk != null) jdk)
     ++ (stdenv.lib.optional (gnuplot != null) gnuplot)
     ++ (stdenv.lib.optional (python != null) python)
+    ++ (stdenv.lib.optional (osmesa != null) mesa_noglu.osmesa)
     ;
+
+
+  preConfigure = stdenv.lib.optionalString (osmesa != null) ''
+    export LLVM_CONFIG=${selected_llvm}/bin/llvm-config
+  '';
 
   doCheck = true;
 
   enableParallelBuilding = true;
+
+  patches = stdenv.lib.optional (osmesa != null) [ ./configure-enable-llvm.patch ];
 
   configureFlags =
     [ "--enable-readline"
@@ -55,12 +65,13 @@ stdenv.mkDerivation rec {
   # Keep a copy of the octave tests detailed results in the output
   # derivation, because someone may care
 
-  # Add gnuplot to PATH if specified as build input.
+  # Add gnuplot and ghostscript to PATH if specified as build input.
+  wrapList = (stdenv.lib.optional (gnuplot != null) gnuplot)
+           ++(stdenv.lib.optional (ghostscript != null) ghostscript);
   postInstall = ''
     cp test/fntests.log $out/share/octave/${name}-fntests.log || true
-  '' + stdenv.lib.optionalString (gunplot != null) ''
-    wrapProgram $out/bin/octave --suffix PATH : ${gnuplot}/bin
-  '';
+  '' + stdenv.lib.optionalString (wrapList != []) ''
+    wrapProgram $out/bin/octave --suffix PATH : ${stdenv.lib.makeBinPath wrapList}'';
 
   passthru = {
     inherit version;
