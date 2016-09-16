ae649d70c04ae6ba0165dece0c7003ba35bc3f44
timor <timor.dd@googlemail.com>
2016-09-16 15:50:29 +0200

    gl2ps: 1.3.8 -> 1.3.9
    fixes octave pgf output

--- pkgs/development/libraries/gl2ps/default.nix
+++ pkgs/development/libraries/gl2ps/default.nix
@@ -2,11 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "gl2ps-${version}";
-  version = "1.3.8";
+  version = "1.3.9";
 
   src = fetchurl {
     url = "http://geuz.org/gl2ps/src/gl2ps-${version}.tgz";
-    sha256 = "1r8y62y3sh3algs02jcmjw3vygh90cc0prw8q6l8hrphbpcqvr9g";
+    sha256 = "0h1nrhmkc4qjw2ninwpj2zbgwhc0qg6pdhpsibbvry0d2bzhns4a";
+    # sha256 = "1r8y62y3sh3algs02jcmjw3vygh90cc0prw8q6l8hrphbpcqvr9g";
   };
 
   # documentation wants latex, remove for simplicity
